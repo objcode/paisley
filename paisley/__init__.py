882d3e9069ab93058c4c9fadd71da98924f5e999
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2011-08-28 17:25:21 +0200

    Put back CouchDB in __init__.py
    Given that reactor import is now in __init__(),
    It's fine to keep CouchDB in __init__.py

---
+++ paisley/__init__.py
@@ -0,0 +1,9 @@
+# -*- Mode: Python; test-case-name: paisley.test -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+# Copyright (c) 2007-2008
+# See LICENSE for details.
+
+import client
+
+CouchDB = client.CouchDB
